ac91709159b450ef6762816b621fd71a0f094f15
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-09-20 07:48:24 -0300

    limpando index utilizando require_once

--- CRUD-OrientadoAObjetos-PHP/app/libraries/Core/Autoload.php
+++ CRUD-OrientadoAObjetos-PHP/app/libraries/Core/Autoload.php
@@ -1,4 +1,28 @@
 <?php
+$url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
+$arrUrl = explode("/", $url);
+
+$controller = $arrUrl[0];
+$method = $arrUrl[0];
+
+$params = "";
+
+if(!empty($arrUrl[1]))
+{
+  if ($arrUrl[1] != "") {
+    $method = $arrUrl[1];
+  }
+}
+if(!empty($arrUrl[2]))
+{
+  if ($arrUrl[2] != "") {
+    for ($i=2; $i < count($arrUrl); $i++) { 
+      $params .= $arrUrl[$i] . ',';
+    }
+    $params = trim($params, ',');
+  }
+}
+
 spl_autoload_register(function($class){
   if(file_exists(LIBS . 'Core/' . $class . '.php')){
     require_once(LIBS . 'Core/' . $class . '.php');
